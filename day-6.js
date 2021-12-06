df0b7bb293f39af8eda7e22e69ae49ce22ee8326
Vasli Saidmuradov <44390703+VasliSaidmuradov@users.noreply.github.com>
2021-12-07 01:28:56 +0600

    feat: add part 2 result text

--- day-6.js
+++ day-6.js
@@ -52,6 +52,18 @@
 
 // The first half of this puzzle is complete! It provides one gold star: *
 
+
+// --- Part Two ---
+// Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+// After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+// How many lanternfish would there be after 256 days?
+
+// Your puzzle answer was 1595330616005.
+
+// Both parts of this puzzle are complete! They provide two gold stars: **
+
 // INPUT:
 
 const input = [3,5,3,1,4,4,5,5,2,1,4,3,5,1,3,5,3,2,4,3,5,3,1,1,2,1,4,5,3,1,4,5,4,3,3,4,3,1,1,2,2,4,1,1,4,3,4,4,2,4,3,1,5,1,2,3,2,4,4,1,1,1,3,3,5,1,4,5,5,2,5,3,3,1,1,2,3,3,3,1,4,1,5,1,5,3,3,1,5,3,4,3,1,4,1,1,1,2,1,2,3,2,2,4,3,5,5,4,5,3,1,4,4,2,4,4,5,1,5,3,3,5,5,4,4,1,3,2,3,1,2,4,5,3,3,5,4,1,1,5,2,5,1,5,5,4,1,1,1,1,5,3,3,4,4,2,2,1,5,1,1,1,4,4,2,2,2,2,2,5,5,2,4,4,4,1,2,5,4,5,2,5,4,3,1,1,5,4,5,3,2,3,4,1,4,1,1,3,5,1,2,5,1,1,1,5,1,1,4,2,3,4,1,3,3,2,3,1,1,4,4,3,2,1,2,1,4,2,5,4,2,5,3,2,3,3,4,1,3,5,5,1,3,4,5,1,1,3,1,2,1,1,1,1,5,1,1,2,1,4,5,2,1,5,4,2,2,5,5,1,5,1,2,1,5,2,4,3,2,3,1,1,1,2,3,1,4,3,1,2,3,2,1,3,3,2,1,2,5,2]
